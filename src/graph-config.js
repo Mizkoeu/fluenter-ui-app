61e56dd8b9822349de4409a4b2f9c47811df5bc5
Mike Zou <mizou@microsoft.com>
2020-08-04 11:46:49 -0700

    update graph

--- src/graph-config.js
+++ src/graph-config.js
@@ -72,7 +72,7 @@ const SpecialChildShape = (
 
 const EmptyEdgeShape = (
   <symbol viewBox="0 0 50 50" id="emptyEdge">
-    <circle cx="25" cy="25" r="8" fill="currentColor" />
+    <circle cx="25" cy="25" r="18" fill="currentColor" />
   </symbol>
 );
 
